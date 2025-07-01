b2fc06c17c73e5783ccce9db884e2e843fcb681c
vikas052005 <nalmasuvikas512@gmail.com>
2025-07-01 10:14:32 +0530

    Update max.py

--- max.py
+++ max.py
@@ -1,6 +1,6 @@
-a=[3,4,11,55,6,4]
-max=a[0]
-for i in range(len[a]):
-    if a[i]>max:
-        max=a[i]
-print(max)
+a = [3, 4, 11, 55, 6, 4]
+maximum = a[0] 
+for i in range(len(a)):
+    if a[i] > maximum:
+        maximum = a[i]
+print(maximum)
